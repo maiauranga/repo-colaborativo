ce707cd495a4f72b69b13ac875b39013796fd7b6
catalinasalse <csalse@udesa.edu.ar>
2026-03-12 15:52:46 -0300

    agregue funcion

---
+++ funciones_habitos.py
@@ -0,0 +1,49 @@
+def registrar_habitos (): 
+    '''
+    
+
+    Returns
+    -------
+    habitos : lista
+        lista que contiene las actividades realizadas por los usuarios
+
+    '''
+ 
+    habitos = []
+
+    while True:
+        actividad = input("Ingresa una actividad: ")
+        habitos.append(actividad)
+
+        seguir = input("¿Querés agregar otro hábito? (si/no): ")
+
+        if seguir == "no":
+            break
+
+    return habitos
+
+
+def  analizar_habitos (habitos):
+    '''
+    
+
+    Parameters
+    ----------
+    habitos : lista
+     lista que contiene las actividades realizadas por los usuarios
+
+    Returns
+    -------
+    conteo : diccionario
+        Ddiccionario que muestra cuantas veces aparece cada actividad 
+
+    '''
+    conteo = {}
+    
+    for actividad in habitos:
+        if actividad in conteo:
+            conteo[actividad] += 1
+        else:
+            conteo[actividad] = 1
+
+    return conteo
